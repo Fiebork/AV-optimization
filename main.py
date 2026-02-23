f55364c308ae6a20f51c00d479beb6c7062bc814
Mykola <fiebork@gmail.com>
2026-02-23 14:18:56 +0100

    Calculating acceleration using force, changing force over time

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import matplotlib
 matplotlib.use("QtAgg")
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+
+def force(t):
+    return F0 + amplitude * np.sin(omega * t)
 
 dt = 1.0
 steps = 10
@@ -10,18 +14,22 @@ t = 0.0
 x = 0.0
 v = 0.0
 
-a0 = 2.0
-k = 0.2
+m = 1500.0
+F0 = 1000.0
+amplitude = 300.0
+omega = 0.25
 
 data = []
 
 for _ in range(steps + 1):
-    a = a0 + k * t
+    F = force(t)
+    a = F / m
 
     data.append({
         "t": t,
         "x": x,
         "v": v,
+        "F": F,
         "a": a
     })
 
@@ -53,5 +61,13 @@ plt.plot(df["t"], df["a"])
 plt.xlabel("t")
 plt.ylabel("a")
 plt.title("a(t)")
+plt.grid(True)
+plt.show()
+
+plt.figure()
+plt.plot(df["t"], df["F"])
+plt.xlabel("t")
+plt.ylabel("F")
+plt.title("F(t)")
 plt.grid(True)
 plt.show()
